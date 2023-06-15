4dcb6da1fe2a96ecbe288d839b9940a10f2f7e49
Daniela Moreno <dani9292@hotmail.com>
2023-06-15 11:30:17 -0500

    Add testing

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,3 +1,10 @@
 #all?
 #any?
 #filter
+
+def all_method?(arr)
+  arr.each do |ele|
+    return false if !yield(ele)
+  end
+  true
+end
--- my_list.rb
+++ my_list.rb
@@ -3,3 +3,18 @@ class MyList
     @list = []
   end
 end
+
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? {|e| e < 5} # => true
+list.all? {|e| e > 5}#=> false
+
+# Test #any?
+ list.any? {|e| e == 2} #=> true
+list.any? {|e| e == 5} #=> false
+
+# Test #filter
+list.filter {|e| e.even?} #=> [2, 4]
