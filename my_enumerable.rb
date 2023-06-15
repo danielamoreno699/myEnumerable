726c2fe0bf86a1128072085f594061b055866ba4
Daniela Moreno <dani9292@hotmail.com>
2023-06-15 12:08:42 -0500

    Fix linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,25 +1,23 @@
 module MyEnumerable
-    def all?
-      self.each do |e|
-        return false unless yield(e)
-      end
-      true
+  def all?
+    each do |e|
+      return false unless yield(e)
     end
-
-    def any?
-        self.each do |e|
-          return true if yield(e)
-        end
-        false
-      end
-    
-      def filter
-        result = []
-        self.each do |e|
-          result << e if yield(e)
-        end
-        result
-      end
- 
+    true
   end
-  
+
+  def any?
+    each do |e|
+      return true if yield(e)
+    end
+    false
+  end
+
+  def filter
+    result = []
+    each do |e|
+      result << e if yield(e)
+    end
+    result
+  end
+end
--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,4 @@
-
+require_relative 'my_enumerable'
 
 class MyList
   def initialize(*elements)
@@ -7,15 +7,15 @@ class MyList
 
   include MyEnumerable
 
-  def each
-    @list.each { |e| yield(e) }
+  def each(&block)
+    @list.each(&block)
   end
 end
 
 list = MyList.new(1, 2, 3, 4)
 
-puts list.all? { |e| e < 5 }    # Output: true
-puts list.all? { |e| e > 5 }    # Output: false
-puts list.any? { |e| e == 2 }   # Output: true
-puts list.any? { |e| e == 5 }   # Output: false
-puts list.filter { |e| e.even? } # Output: [2, 4]
+puts(list.all? { |e| e < 5 }) # Output: true
+puts(list.all? { |e| e > 5 }) # Output: false
+puts(list.any? { |e| e == 2 }) # Output: true
+puts(list.any? { |e| e == 5 }) # Output: false
+puts(list.filter(&:even?)) # Output: [2, 4]
