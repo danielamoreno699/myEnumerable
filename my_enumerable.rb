ab0c189d1e3eb6a6de413ce26fe9170b984df331
Daniela Moreno <dani9292@hotmail.com>
2023-06-15 11:58:29 -0500

    Add an all?  function in the module MyEnumerable

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -5,20 +5,6 @@ module MyEnumerable
       end
       true
     end
-  
-    def any?
-      self.each do |e|
-        return true if yield(e)
-      end
-      false
-    end
-  
-    def filter
-      result = []
-      self.each do |e|
-        result << e if yield(e)
-      end
-      result
-    end
+ 
   end
   
